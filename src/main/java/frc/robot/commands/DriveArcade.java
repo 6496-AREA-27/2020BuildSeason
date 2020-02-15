78dd44b63bdeeb3f4bc0833da1811b1a59a3450a
Harry <tx20@duke.edu>
2020-02-15 12:16:25 -0500

    added possible joystick code

--- src/main/java/frc/robot/commands/DriveArcade.java
+++ src/main/java/frc/robot/commands/DriveArcade.java
@@ -29,8 +29,10 @@ public class DriveArcade extends Command {
   protected void execute() {
     double moveSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.DRIVER_CONTROLLER_MOVE_AXIS)*-1;
     double rotateSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.DRIVER_CONTROLLER_ROTATE_AXIS);
-  
+    // double joystickMoveSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.JOYSTICK_MOVE_AXIS)*-1;
+    // double joystickRotateSpeed = Robot.m_oi.driverController.getRawAxis(RobotMap.JOYSTICK_ROTATE_AXIS);
     Robot.m_drivetrain.arcadeDrive(moveSpeed, rotateSpeed);
+    // Robot.m_drivetrain.arcadeDrive(joystickMoveSpeed, joystickRotateSpeed);
 
   }
 

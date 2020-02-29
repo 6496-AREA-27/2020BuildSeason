073b042e1cc348a7f9ba49f418899c4c6c021b98
josiah <josiahlaster1@gmail.com>
2020-02-28 21:59:00 -0500

    2nd camera

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -15,6 +15,7 @@ import org.opencv.imgproc.Imgproc;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.vision.VisionThread;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -28,6 +29,7 @@ public class VisionSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
   Thread visionThread;
+  Thread VisionThread2;
 
   @Override
   public void initDefaultCommand() {
@@ -66,5 +68,39 @@ public class VisionSubsystem extends Subsystem {
 		});
 		visionThread.setDaemon(true);
 		visionThread.start();
+	VisionThread2 = new Thread(() -> {
+			// Get the UsbCamera from CameraServer
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+			// Set the resolution
+		camera.setResolution(640, 480);
+
+			// Get a CvSink. This will capture Mats from the camera
+		CvSink cvSink = CameraServer.getInstance().getVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+		CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+			// Mats are very memory expensive. Lets reuse this Mat.
+		Mat mat = new Mat();
+
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+		while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat. If there is an error notify the output.
+			if (cvSink.grabFrame(mat) == 0) {
+					// Send the output the error.
+				outputStream.notifyError(cvSink.getError());
+					// skip the rest of the current iteration
+				continue;
+			}
+				// Put a rectangle on the image
+			Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+			outputStream.putFrame(mat);
+		}
+		});
+		VisionThread2.setDaemon(true);
+		VisionThread2.start();
     }
 }
